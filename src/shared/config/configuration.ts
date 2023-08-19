ed18e7b8a3c6b28568a859fb0bda7cd4b7fe0453
Leonardo Nakao <leonakao.dev@gmail.com>
2023-08-19 20:41:36 -0300

    chore: update database configuration

--- src/shared/config/configuration.ts
+++ src/shared/config/configuration.ts
@@ -1,11 +1,14 @@
 export default () => ({
-    port: parseInt(process.env.PORT, 10) || 3000,
-    database: {
-      type: process.env.DATABASE_DRIVER,
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      name: process.env.DATABASE_NAME,
-    }
-  });
+  port: parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    type: process.env.DATABASE_DRIVER,
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [__dirname + '/../../modules/**/entities/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+    migrationsTableName: 'migrations_typeorm',
+  },
+});
--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import configuration from '../config/configuration';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { config } from 'process';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-        imports: [ConfigModule],
-        useFactory: (config: ConfigService) => config.get('database'),
-        inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => config.get('database'),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
