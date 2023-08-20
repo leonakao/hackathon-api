c78bdf0a9648265dc1f21595eb3e4e42f0c81464
Leonardo Nakao <leonakao.dev@gmail.com>
2023-08-20 06:54:55 -0300

    feat: add summary into the queue

--- src/modules/summary/handlers/store.handler.ts
+++ src/modules/summary/handlers/store.handler.ts
@@ -4,11 +4,12 @@ import { SummaryRepository } from '../repositories/summary.repository';
 import { ProfileRepository } from '../repositories/profile.repository';
 import { FileInputRepository } from '../repositories/fileInput.repository';
 import { randomUUID } from 'crypto';
-import { FileInputType } from '../enums';
+import { FileInputType, QueueStatus } from '../enums';
 import { SummaryStatus } from '../enums/summaryStatus.enum';
 import { AuthenticatedUser } from 'src/modules/auth/services/signIn.service';
 import { FindProtectedGroupService } from 'src/modules/group/services/findProtectedGroup.service';
 import { AddSummaryToGroupService } from 'src/modules/group/services/addSummaryToGroup.service';
+import { QueueRepository } from '../repositories/queue.repository';
 
 export interface SummaryStoreDto {
   title: string;
@@ -22,9 +23,13 @@ export interface SummaryStoreDto {
 
 @Injectable()
 export class SummaryStoreHandler {
+  private INITIAL_RETRIES = 0;
+  private MAX_RETRIES = 3;
+
   constructor(
     private readonly summaryRepository: SummaryRepository,
     private readonly profileRepository: ProfileRepository,
+    private readonly queueRepository: QueueRepository,
     private readonly fileInputRepository: FileInputRepository,
     private readonly findProtectedGroup: FindProtectedGroupService,
     private readonly addSummaryToGroup: AddSummaryToGroupService,
@@ -59,6 +64,14 @@ export class SummaryStoreHandler {
       publicUrl: data.link,
       createdBy: user.id,
       type: inputType,
+    });
+
+    await this.queueRepository.store({
+      id: randomUUID(),
+      summaryId: summary.id,
+      retries: this.INITIAL_RETRIES,
+      maxRetries: this.MAX_RETRIES,
+      status: QueueStatus.PENDING,
     });
 
     const groupId = await this.resolveGroupId(data, user.id);
