d13b2773fa982f0968814672041c9e2989698af9
Leonardo Nakao <leonakao.dev@gmail.com>
2023-08-19 22:40:35 -0300

    refactor: remove bcrypt

--- src/shared/encrypt/services/encrypt.service.ts
+++ src/shared/encrypt/services/encrypt.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class EncryptService {
   async hash(value: string): Promise<string> {
-    const salt = await bcrypt.genSalt();
-    return await bcrypt.hash(value, salt);
+    return new Promise((resolve, reject) => {
+      const salt = crypto.randomBytes(16).toString('hex');
+
+      crypto.scrypt(value, salt, 64, (err, derivedKey) => {
+        if (err) reject(err);
+        resolve(salt + ':' + derivedKey.toString('hex'));
+      });
+    });
   }
 
   async compare(value: string, hash: string): Promise<boolean> {
-    return await bcrypt.compare(value, hash);
+    return new Promise((resolve, reject) => {
+      const [salt, key] = hash.split(':');
+
+      crypto.scrypt(value, salt, 64, (err, derivedKey) => {
+        if (err) reject(err);
+        resolve(key == derivedKey.toString('hex'));
+      });
+    });
   }
 }
